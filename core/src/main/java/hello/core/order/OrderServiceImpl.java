81326cda795417e5fd61bdf2efaf0d9f1bafb556
공대표 <c5ngsdp@gmail.com>
2024-02-15 13:53:59 +0900

    Section 2 - 주문과 할인 도메인 개발

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderSerivce {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member findMember = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(findMember, itemPrice); /* 할인에 대한건 discountPolicy에 맡김 즉 SRP를 잘 지켰다. */
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
